f082f1731cd9e2cc624e748f8ff3bad05ce57b4c
rboissel <raphael.boissel@orange.fr>
2018-06-02 15:50:31 -0700

    Fix the assembly property to remove the .NET revision from the name

--- ratchet-wgl/Build/ratchet-wgl-net40/Properties/AssemblyInfo.cs
+++ ratchet-wgl/Build/ratchet-wgl-net40/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("ratchet-wgl-net40")]
+[assembly: AssemblyTitle("ratchet-wgl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ratchet-wgl-net40")]
+[assembly: AssemblyProduct("ratchet-wgl")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
